b03aad6652177b4b2fb1304cab420f56ffc80842
Malik Laet <isklikma@gmail.com>
2024-12-10 18:18:29 -0300

    add router

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,14 @@
-import Home from "./home"
 import React from "react"
+import { BrowserRouter} from "react-router-dom"
+import Router from "./router"
 export function App() {
 
   return (
+    <BrowserRouter>
     <div className="bg-background">
-     <Home/>
+      <Router/>
     </div>
+    </BrowserRouter>
   )
 }
 
